62f5ef3646ff86930ae108938ad17d0778a0e048
Steven H. Wang <wang.steven.h@gmail.com>
2022-05-30 19:27:24 +0200

    install_polyjuice: Fix poetry shell problem

--- install_polyjuice.sh
+++ install_polyjuice.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-poetry shell
 git clone https://github.com/tongshuangwu/polyjuice.git
 cd polyjuice
-pip install -e .
-python -m spacy download en_core_web_sm
-python -c "import nltk; nltk.download('omw-1.4')"
+pr="poetry run"
+$pr pip install -e .
+$pr python -m spacy download en_core_web_sm
+$pr python -c "import nltk; nltk.download('omw-1.4')"
