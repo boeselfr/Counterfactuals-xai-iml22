277c42163b80c5b5e7ec4c2cfa32ab6ee1b029e7
boeselfr <boesel.frederic@googlemail.com>
2022-06-02 11:58:03 +0200

    stroing mmodel ßredictions for each submitted cf

--- backend/roberta_inference.py
+++ backend/roberta_inference.py
@@ -21,9 +21,9 @@ def roberta_probability(sentence1: str,
     encoded_input = tokenizer([(sentence1, sentence2)], padding=True, truncation=True, return_tensors='pt')
     with torch.no_grad():
         logits = model(**encoded_input)[0]  ## select the string in returned list
-    softmax = torch.nn.Softmax(dim=0)
-    probs = softmax(logits).detach().cpu().numpy()
+    softmax = torch.nn.Softmax(dim=1)
+    probs = softmax(logits).detach().cpu().numpy()[0]
     prob_dict = dict()
-    for i in range(len(logits)):
+    for i in range(3):
         prob_dict[model.config.id2label[i]] = probs[i]
     return prob_dict
